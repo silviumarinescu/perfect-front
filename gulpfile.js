0ae90e9a1c720c64f4cf9e4f10644f68f1131a22
Silviu Marinescu <f.silviumarinescu@gmail.com>
2021-03-26 10:52:25 +0200

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,38 @@
-var gulp = require("gulp");
-var handlebars = require("gulp-compile-handlebars");
-var rename = require("gulp-rename");
+const gulp = require("gulp");
+const handlebars = require("gulp-compile-handlebars");
+const rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
 
-gulp.task("default", function () {
-  var templateData = {
-      firstName: "Kaanon",
-    },
-    options = {
-      ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
-    
-      batch: ["./src/partials"],
-      helpers: {
-        capitals: function (str) {
-          return str.toUpperCase();
-        },
-      },
-    };
+const templateData = {
+  firstName: "Kaanon",
+};
 
-  return gulp
-    .src("src/hello.handlebars")
-    .pipe(handlebars(templateData, options))
-    .pipe(rename("hello.html"))
-    .pipe(gulp.dest("dist"));
+const hbs = () =>
+  gulp
+    .src("src/pages/*.hbs")
+    .pipe(
+      handlebars(templateData, {
+        ignorePartials: true,
+        batch: ["./src/components"],
+      })
+    )
+    .pipe(rename({ extname: ".html" }))
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream());
+
+const styles = () =>
+  gulp
+    .src("src/styles/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("dist/styles"))
+    .pipe(browserSync.stream());
+
+gulp.task("default", () => {
+  browserSync.init({
+    server: "./dist",
+  });
+  gulp.watch("src/styles/*.scss", styles);
+  gulp.watch("src/**/*.hbs", hbs);
+  gulp.watch("dist/*.html").on("change", browserSync.reload);
 });
