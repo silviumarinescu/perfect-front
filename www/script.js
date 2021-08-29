992c0ededc066ff36249dbda67f78a1db0db8635
Silviu Marinescu <f.silviumarinescu@gmail.com>
2021-08-29 14:39:21 +0300

    wip

--- www/script.js
+++ www/script.js
@@ -7,4 +7,25 @@ window.onload = () => {
       pager: true,
       slideWidth: 600,
     });
+
+  $(".gallery").on("click", () => {
+    let gallery = new PhotoSwipe(
+      $(".pswp")[0],
+      PhotoSwipeUI_Default,
+      [
+        {
+          src: "https://unsplash.it/1200/900/?image=702",
+          w: "1200",
+          h: "900",
+          title: "test",
+        },
+      ],
+      {
+        index: 0,
+        bgOpacity: 0.85,
+        showHideOpacity: true,
+      }
+    );
+    gallery.init();
+  });
 };
